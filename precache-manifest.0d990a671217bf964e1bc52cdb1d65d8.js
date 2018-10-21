023fe3725c7bfa91367e6ae9767311db5ada6b78
brauliodeleon <hi@brauliodeleon.com>
2018-10-21 10:37:48 -0300

    Updates

---
+++ precache-manifest.0d990a671217bf964e1bc52cdb1d65d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "eb8de8871d3dfde82627",
+    "url": "/gh-react-app/static/js/runtime~main.eb8de887.js"
+  },
+  {
+    "revision": "f94bbe9609b16738ef31",
+    "url": "/gh-react-app/static/js/main.f94bbe96.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/gh-react-app/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "f94bbe9609b16738ef31",
+    "url": "/gh-react-app/static/css/main.fe172616.chunk.css"
+  },
+  {
+    "revision": "0f535d1bfb3d4e31ea6e88ffdb9161fd",
+    "url": "/gh-react-app/index.html"
+  }
+];
